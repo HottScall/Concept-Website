80152c856a60b28864640e4621a393191e8723dd
Scott <Scotthall1983@hotmail.com>
2020-01-14 23:59:21 +0000

    Imported the React Router in App.js and added the relevant routes and components required for the site. Added the components as a base structure for the site.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route path="/about" component={about} />
+          <Route path="/projects" component={projects} />
+          <Route path="/contact" component={contact} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
 }
 
 export default App;
