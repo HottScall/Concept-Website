67ea6b97b788975c40361accf6b9ee0a139fa8a1
Scott <Scotthall1983@hotmail.com>
2020-01-15 06:14:34 +0000

    Added exerpts from a book into the home and about pages just to ensure that the pages scroll down and content remains in tact

--- src/About.js
+++ src/About.js
@@ -3,7 +3,57 @@ import React from "react";
 export const About = () => (
   <div>
     <h1>About</h1>
-    <p>blah blah blah blah blah blah blah blah blah blah blah blah</p>
-    <p>blah blah blah blah blah blah blah blah blah blah blah blah</p>
+    <p>I don’t have a brother or sister. I want one.</p>
+    <p>
+      Tell me, lady, how could it be that I wouldn’t be born? Where would I be?
+      High in the sky? On another planet?
+    </p>
+    <p>
+      The sparrows disappeared from our town in the first year after the
+      accident. They were lying around everywhere – in the yards, on the
+      asphalt. They’d be raked up and taken away in the containers with the
+      leaves. They didn’t let people burn the leaves that year, because they
+      were radioactive, so they buried the leaves.
+    </p>
+    <p>
+      The sparrows came back two years later. We were so happy, we were calling
+      to each other: “I saw a sparrow yesterday! They’re back.”
+    </p>
+    <p>
+      The May bugs also disappeared, and they haven’t come back. Maybe they’ll
+      come back in a hundred years or a thousand. That’s what our teacher says.
+      I won’t see them.
+    </p>
+    <p>
+      September first, the first day of school, and there wasn’t a single
+      flower. The flowers were radioactive. Before the beginning of the year,
+      the people working weren’t masons, like before, but soldiers. They mowed
+      the flowers, took off the earth and took it away somewhere in cars with
+      trailers.
+    </p>
+    <p>
+      In a year they evacuated all of us and buried the village. My father’s a
+      cab driver, he drove there and told us about it. First they’d tear a big
+      pit in the ground, five meters deep. Then the firemen would come up and
+      use their hoses to wash the house from its roof to its foundation, so that
+      no radioactive dust gets kicked up. They wash the windows, the roof, the
+      door, all of it. Then a crane drags the house from its spot and puts it
+      down into the pit. There’s dolls and books and cans all scattered around.
+      The excavator picks them up. Then it covers everything with sand and clay,
+      leveling it. And then instead of a village, you have an empty field. They
+      sowed our land with corn. Our house is lying there, and our school and our
+      village council office. My plants are there and two albums of stamps, I
+      was hoping to bring them with me. Also I had a bike.
+    </p>
+    <p>
+      I’m twelve years old and I’m an invalid. The mailman brings two pension
+      checks to our house – for me and my granddad. When the girls in my class
+      found out that I had cancer of the blood, they were afraid to sit next to
+      me. They didn’t want to touch me.
+    </p>
+    <p>
+      The doctors said that I got sick because my father worked at Chernobyl.
+      And after that I was born. I love my father.
+    </p>
   </div>
 );
