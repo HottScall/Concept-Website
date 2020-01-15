d36bf4e1e4d960a157fa2b59953ef88b70f0dc8d
Scott <Scotthall1983@hotmail.com>
2020-01-15 02:58:09 +0000

    Added the basic structure for a contact form, this is initially for display purposes only.

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,16 @@ import React from "react";
 export const Contact = () => (
   <div>
     <h1>Contact Us</h1>
-    <p>blah blah blah blah blah blah blah blah blah blah blah blah</p>
-    <p>blah blah blah blah blah blah blah blah blah blah blah blah</p>
+    <form>
+      <label>Name</label>
+      <input type="text"></input>
+      <label>Contact Number</label>
+      <input type="text"></input>
+      <label>Email Address</label>
+      <input type="text"></input>
+      <label>Enquiry Description</label>
+      <textarea rows="6" cols="50"></textarea>
+      <input type="submit"></input>
+    </form>
   </div>
 );
