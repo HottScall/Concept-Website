ac960f201b6ad06558895b7ace817679d27821a6
Scott <Scotthall1983@hotmail.com>
2020-02-03 19:22:02 +0000

    Created another Jumbotron which contains the social media elements

--- src/components/HomeSocial.js
+++ src/components/HomeSocial.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Jumbotron, Button } from "react-bootstrap";
+import { Jumbotron } from "react-bootstrap";
 import styled from "styled-components";
 
 const Styles = styled.div`
@@ -9,19 +9,41 @@ const Styles = styled.div`
     color: #ffffff;
     margin-top: 10px;
   }
+
+  #icon {
+    padding: 2.5%;
+  }
 `;
 
 export const HomeSocial = () => (
   <Styles>
     <div className="home-social-wrapper">
       <Jumbotron className="Jumbo">
-        <h1 align="center">See me on social media</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
+        <h1 align="center">See our work on social media</h1>
+        <p align="center">
+          <a
+            href="https://www.instagram.com/lasadler_carpentry/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i id="icon" className="fab fa-instagram fa-7x"></i>
+          </a>
+
+          <a
+            href="https://www.facebook.com/lasadlercarpentry/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i id="icon" className="fab fa-facebook-square fa-7x"></i>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/luke-sadler-625ba7103/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i id="icon" className="fab fa-linkedin fa-7x"></i>
+          </a>
         </p>
       </Jumbotron>
     </div>
